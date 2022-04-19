82f0aeac6fb4ecb000ee4260e8f07bf63a0e1851
Iasmina <iasminamaria99@gmail.com>
2022-04-19 14:32:48 +0300

    add login and register

---
+++ app/src/main/java/com/example/authentication/Register.java
@@ -0,0 +1,92 @@
+package com.example.authentication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.splashscreen.Dashboard;
+import com.example.splashscreen.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+    EditText mName, mEmail, mPassword;
+    Button mRegisterButton;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mName = findViewById(R.id.name);
+        mEmail = findViewById(R.id.EmailAdress);
+        mPassword = findViewById(R.id.password);
+        mRegisterButton = findViewById(R.id.registerButton);
+        mLoginBtn = findViewById(R.id.textView4);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), Dashboard.class));
+            finish();
+
+        }
+
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is required.");
+                    return;
+                }
+                if (password.length() < 6) {
+                    mPassword.setError("Password must have more than 5 characters.");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                //REGISTER THE USER IN FIREBASE
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register.this, "Account created!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Dashboard.class));
+                        } else {
+                            Toast.makeText(Register.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/splashscreen/SplashScreen.java
+++ app/src/main/java/com/example/splashscreen/SplashScreen.java
@@ -1,4 +1,52 @@
 package com.example.splashscreen;
 
-public class SplashScreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.authentication.Login;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_SCREEN =3000;
+
+   Animation topAnim, bottomAnim;
+   TextView logo, slogan;
+   ImageView image1, image2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        logo = findViewById(R.id.textView4);
+        slogan = findViewById(R.id.textView5);
+        image1 = findViewById(R.id.imageView3);
+        image2 = findViewById(R.id.imageView4);
+
+
+        logo.setAnimation(topAnim);
+        slogan.setAnimation(bottomAnim);
+        image1.setAnimation(topAnim);
+        image2.setAnimation(bottomAnim);
+
+       new Handler().postDelayed(new Runnable() {
+           @Override
+           public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish();
+           }
+       },SPLASH_SCREEN);
+
+    }
 }
